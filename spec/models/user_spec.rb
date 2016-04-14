aca558b715192c20405e563bd444f5cef5b16814
Matt Pindell <pindell.m@gmail.com>
2016-04-14 14:57:15 -0600

    Add User model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "validations" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:username) }
+  end
+end

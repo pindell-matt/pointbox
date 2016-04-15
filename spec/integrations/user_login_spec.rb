330fdebca102eed58860e6ceee486366b62c8f16
Matt Pindell <pindell.m@gmail.com>
2016-04-15 10:04:41 -0600

    Add Factory Girl and update tests

--- spec/integrations/user_login_spec.rb
+++ spec/integrations/user_login_spec.rb
@@ -1,18 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "User logs in to website" do
-  scenario "existing user can login" do
-    User.create(username: "Clarence",
-                password: "password")
+  scenario "user creates account and logs in" do
+    visit new_user_path
 
-    visit login_path
-    fill_in "Username", with: "Clarence"
-    fill_in "Password", with: "password"
-    click_button "Login"
+    fill_in "username", with: "Fakename"
+    fill_in "password", with: "password"
+    click_button "Create Account"
 
-    expect(page).to have_content("User Show Page")
+    expect(page).to have_content("Welcome, Fakename")
     expect(page).to have_content("Logout")
     expect(page).to have_content("My Spent Points")
     expect(page).to have_content("My Unspent Points")
   end
+
+  scenario "existing user can login" do
+    user = create(:user)
+
+    visit login_path
+    fill_in "username", with: "Clarence"
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    expect(page).to have_content("Welcome, Clarence")
+    expect(page).to have_content("Logout")
+    expect(page).to have_content("My Spent Points")
+    expect(page).to have_content("My Unspent Points")
+  end
+
+  # sad path
+  # context "the submitted login data is invalid" do
 end

531fcfcbe8d08484e5656bc64e0889bdd5525acc
Matt Pindell <pindell.m@gmail.com>
2016-04-14 19:15:55 -0600

    Add create user and user login functionality

---
+++ spec/integrations/user_login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "User logs in to website" do
+  scenario "existing user can login" do
+    User.create(username: "Clarence",
+                password: "password")
+
+    visit login_path
+    fill_in "Username", with: "Clarence"
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    expect(page).to have_content("User Show Page")
+    # expect(page).to have_content("Log Out")
+    # expect(page).to have_content("My Spent Points")
+    # expect(page).to have_content("My Unspent Points")
+  end
+end

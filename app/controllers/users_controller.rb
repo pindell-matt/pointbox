09aadc45ae7474d2e611f4d4ae93df5201a457f6
Matt Pindell <pindell.m@gmail.com>
2016-04-15 10:18:18 -0600

    Add sad path testing for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
+      flash.now[:error] = "Invalid. Try Again."
       render :new
     end
   end
--- spec/integrations/user_login_spec.rb
+++ spec/integrations/user_login_spec.rb
@@ -1,33 +1,103 @@
 require 'rails_helper'
 
 RSpec.feature "User logs in to website" do
-  scenario "user creates account and logs in" do
-    visit new_user_path
+  context "the submitted data is valid" do
+    scenario "user creates account and logs in" do
+      visit new_user_path
 
-    fill_in "username", with: "Fakename"
-    fill_in "password", with: "password"
-    click_button "Create Account"
+      fill_in "username", with: "Fakename"
+      fill_in "password", with: "password"
+      click_button "Create Account"
 
-    expect(page).to have_content("Welcome, Fakename")
-    expect(page).to have_content("Logout")
-    expect(page).to have_content("My Spent Points")
-    expect(page).to have_content("My Unspent Points")
+      expect(page).to have_content("Welcome, Fakename")
+      expect(page).to have_content("Logout")
+      expect(page).to have_content("My Spent Points")
+      expect(page).to have_content("My Unspent Points")
+    end
+
+    scenario "existing user can login" do
+      user = create(:user)
+
+      visit login_path
+      fill_in "username", with: "Clarence"
+      fill_in "password", with: "password"
+      click_button "Login"
+
+      expect(page).to have_content("Welcome, Clarence")
+      expect(page).to have_content("Logout")
+      expect(page).to have_content("My Spent Points")
+      expect(page).to have_content("My Unspent Points")
+    end
   end
 
-  scenario "existing user can login" do
-    user = create(:user)
+  context "the submitted data is invalid" do
+    scenario "no username provided on create account" do
+      visit new_user_path
 
-    visit login_path
-    fill_in "username", with: "Clarence"
-    fill_in "password", with: "password"
-    click_button "Login"
+      fill_in "password", with: "password"
+      click_button "Create Account"
 
-    expect(page).to have_content("Welcome, Clarence")
-    expect(page).to have_content("Logout")
-    expect(page).to have_content("My Spent Points")
-    expect(page).to have_content("My Unspent Points")
+      within("#flash_error") do
+        expect(page).to have_content("Invalid. Try Again.")
+      end
+    end
+
+    scenario "no password provided on create account" do
+      visit new_user_path
+
+      fill_in "username", with: "Clarence"
+      click_button "Create Account"
+
+      within("#flash_error") do
+        expect(page).to have_content("Invalid. Try Again.")
+      end
+    end
+
+    scenario "no username provided on login" do
+      visit login_path
+
+      fill_in "password", with: "password"
+      click_button "Login"
+
+      within("#flash_error") do
+        expect(page).to have_content("Invalid. Try Again.")
+      end
+    end
+
+    scenario "no password provided on login" do
+      visit login_path
+
+      fill_in "username", with: "Clarence"
+      click_button "Login"
+
+      within("#flash_error") do
+        expect(page).to have_content("Invalid. Try Again.")
+      end
+    end
+
+    scenario "wrong password provided on login" do
+      visit login_path
+
+      fill_in "username", with: "Clarence"
+      fill_in "password", with: "wrong"
+      click_button "Login"
+
+      within("#flash_error") do
+        expect(page).to have_content("Invalid. Try Again.")
+      end
+    end
+
+    scenario "wrong username provided on login" do
+      visit login_path
+
+      fill_in "username", with: "Not In System"
+      fill_in "password", with: "password"
+      click_button "Login"
+
+      within("#flash_error") do
+        expect(page).to have_content("Invalid. Try Again.")
+      end
+    end
+
   end
-
-  # sad path
-  # context "the submitted login data is invalid" do
 end

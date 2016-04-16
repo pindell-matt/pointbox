522e6fffdbde8a386a5055c86df4ab4eb62a0902
Matt Pindell <pindell.m@gmail.com>
2016-04-16 11:54:32 -0600

    Test admin_creates_reward_spec passing

--- spec/features/admin_creates_reward_spec.rb
+++ spec/features/admin_creates_reward_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "Admin creates reward" do
   scenario "with valid input" do
-    reward = create(:reward)
     admin  = User.create(username: "admin",
                          password: "password",
                          role:     1)
@@ -12,13 +11,14 @@ RSpec.feature "Admin creates reward" do
     fill_in "password", with: "password"
     click_button "Login"
 
-    click_link "Create New Reward"
+    click_link "Rewards Shop"
 
-    fill_in "name", with: reward.name
-    fill_in "value", with: reward.value
+    click_button "Create New Reward"
+
+    fill_in "name", with: "Test-Trophy"
+    fill_in "value", with: 5
     click_button "Create Reward"
 
-    # save_and_open_page
     expect(page).to have_content("Reward Created!")
   end
 end

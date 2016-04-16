e11ebaea9db405ae43b6271520bc8fedd7f01129
Matt Pindell <pindell.m@gmail.com>
2016-04-16 16:50:56 -0600

    Add total points to all relevant views

--- spec/features/admin_can_assign_points_to_user_spec.rb
+++ spec/features/admin_can_assign_points_to_user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Admin can assign points to user" do
-  scenario "assigns points to existing user" do
+  scenario "with valid params" do
     user   = create(:user)
     admin  = User.create(username: "admin",
                          password: "password",
@@ -15,6 +15,24 @@ RSpec.feature "Admin can assign points to user" do
     click_link "Assign Points"
     fill_in "value", with: 5
     fill_in "user id", with: user.id
+    click_button "Assign"
+
+    expect(page).to have_content "#{user.username}: 5 points"
+  end
+
+  scenario "with invalid params" do
+    user   = create(:user)
+    admin  = User.create(username: "admin",
+                         password: "password",
+                         role:     1)
+
+    visit login_path
+    fill_in "username", with: "admin"
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    click_link "Assign Points"
+    fill_in "value", with: 5
     click_button "Assign"
 
     expect(page).to have_content "#{user.username}: 5 points"

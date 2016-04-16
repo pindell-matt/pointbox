95f3a08550ea66d072c349034fb73b61bef57b44
Matt Pindell <pindell.m@gmail.com>
2016-04-16 12:27:19 -0600

    Add Admin can delete reward test

---
+++ spec/features/admin_can_delete_reward_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Admin can delete reward" do
+  scenario "deletes pre-existing reward" do
+    reward = create(:reward)
+    admin  = User.create(username: "admin",
+                         password: "password",
+                         role:     1)
+
+    visit login_path
+    fill_in "username", with: "admin"
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    click_link "Rewards Shop"
+
+    expect(page).to have_content("#{reward.name}: #{reward.value} points")
+    expect(page).to have_button("Edit")
+    expect(page).to have_button("Delete")
+
+    click_button "Delete"
+
+    expect(page).to have_content("Rewards Index")
+    expect(page).to_not have_content("#{reward.name}: #{reward.value} points")
+    expect(page).to_not have_button("Edit")
+    expect(page).to_not have_button("Delete")
+  end
+end

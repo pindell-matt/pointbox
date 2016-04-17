e53abe5c45bc7ef78be684c3b356826e8d24bdbf
Matt Pindell <pindell.m@gmail.com>
2016-04-17 17:28:18 -0600

    Add full test coverage for user purchases

--- spec/features/user_can_purchase_reward_spec.rb
+++ spec/features/user_can_purchase_reward_spec.rb
@@ -11,19 +11,62 @@ RSpec.feature "User can purchase reward with points" do
     fill_in "password", with: "password"
     click_button "Login"
 
-    # click_link "Get Rewards"
+    click_link "Purchase A Reward"
 
+    select('Trophy: 0 points', from: 'user_rewards')
 
-    # expect(page).to have_content "Points Assigned!"
-    # expect(page).to have_content "User #{user.id}: #{user.username}"
-    # expect(page).to have_content "Total Points: 5"
+    click_button "Purchase Reward"
+
+    expect(page).to have_content "Reward Purchased!"
+
+    within(".user_rewards") do
+      expect(page).to have_content "Trophy"
+    end
   end
 
   scenario "with 1 point reward" do
+    user   = create(:user)
+    reward = Reward.create(name:  "Expensive Trophy",
+                           value: 1)
 
+    user.points.create(value: 1)
+
+    visit login_path
+    fill_in "username", with: user.username
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    click_link "Purchase A Reward"
+
+    select('Expensive Trophy: 1 points', from: 'user_rewards')
+
+    click_button "Purchase Reward"
+
+    expect(page).to have_content "Reward Purchased!"
+    expect(page).to have_content "Points: 0"
+    expect(page).to have_content "Spent: 1"
+
+    within(".user_rewards") do
+      expect(page).to have_content "Expensive Trophy"
+    end
   end
 
   scenario "without enough points" do
+    user   = create(:user)
+    reward = Reward.create(name:  "Expensive Trophy",
+                           value: 1)
 
+    visit login_path
+    fill_in "username", with: user.username
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    click_link "Purchase A Reward"
+
+    select('Expensive Trophy: 1 points', from: 'user_rewards')
+
+    click_button "Purchase Reward"
+
+    expect(page).to have_content "Insufficient Funds, Yo!"
   end
 end

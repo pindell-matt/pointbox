f724277994694175ab85b90a1a234dfb95ea04f9
Matt Pindell <pindell.m@gmail.com>
2016-04-17 18:06:28 -0600

    Add Admin full User CRUD

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,4 @@
 class Admin::UsersController < Admin::BaseController
-  # before_action :set_reward, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -9,46 +8,16 @@ class Admin::UsersController < Admin::BaseController
     @user = User.find(params[:id])
   end
 
+  def create
+  end
 
-#   def new
-#     @reward = Reward.new
-#   end
-#
-#   def create
-#     @reward = Reward.new(reward_params)
-#     if @reward.save
-#       flash[:notice] = "Reward Created!"
-#       redirect_to admin_rewards_path
-#     else
-#       render :new
-#     end
-#   end
-#
-#   def edit
-#   end
-#
-#   def update
-#     if @reward.update(reward_params)
-#       flash[:notice] = "Reward Updated!"
-#       redirect_to admin_rewards_path
-#     else
-#       render :edit
-#     end
-#   end
-#
-#   def destroy
-#     @reward.destroy
-#     redirect_to admin_rewards_path
-#   end
-#
-#   private
-#
-#   def reward_params
-#     params.require(:reward).permit(:name, :value)
-#   end
-#
-#   def set_reward
-#     @reward = Reward.find(params[:id])
-#   end
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 
 end

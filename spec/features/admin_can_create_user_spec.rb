273d046eccd98834897e4891f793503c630ec0ff
Matt Pindell <pindell.m@gmail.com>
2016-04-17 19:11:57 -0600

    Remove comments from tests

--- spec/features/admin_can_create_user_spec.rb
+++ spec/features/admin_can_create_user_spec.rb
@@ -19,28 +19,31 @@ RSpec.feature "Admin creates user" do
     fill_in "password", with: "password"
     click_button "Create User"
 
-    expect(page).to have_content("User Created!")
+    expect(page).to have_content("User Successfully Created!")
     expect(page).to have_content("DIY User")
   end
 
-  # scenario "with invalid input" do
-  #   admin  = User.create(username: "admin",
-  #                        password: "password",
-  #                        role:     1)
-  #
-  #   visit login_path
-  #   fill_in "username", with: "admin"
-  #   fill_in "password", with: "password"
-  #   click_button "Login"
-  #
-  #   click_link "Rewards Gallery"
-  #
-  #   click_button "Create New Reward"
-  #
-  #   fill_in "name", with: "Test-Trophy"
-  #   click_button "Create Reward"
-  #
-  #   expect(page).to have_content("New Reward")
-  # end
+  scenario "with invalid input" do
+    admin  = User.create(username: "admin",
+                         password: "password",
+                         role:     1)
+
+    visit login_path
+    fill_in "username", with: "admin"
+    fill_in "password", with: "password"
+    click_button "Login"
+
+    click_link "Users Index"
+
+    click_button "Create New User"
+
+    fill_in "username", with: "DIY User"
+    click_button "Create User"
+
+    expect(page).to have_content("Invalid Submission!")
+    expect(page).to_not have_content("DIY User")
+  end
+
+
 
 end
